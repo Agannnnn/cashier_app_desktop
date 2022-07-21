ca81819cdb6cce88ce19d42aa48fb149b74873f8
Agannnnn <manusia0.5gabus@gmail.com>
2022-07-21 11:15:11 +0700

    Changing database import file

--- database.sql
+++ database.sql
@@ -1,59 +1,170 @@
-CREATE DATABASE projek_kasir_java;
-USE projek_kasir_java;
--- STATUS:
---  0 = TIDAK AKTIF
---  1 = AKTIF
-CREATE TABLE kasir (
-  ID VARCHAR(12) NOT NULL,
-  password VARCHAR(30) NOT NULL,
-  nama VARCHAR(50) NOT NULL,
-  jk CHAR(1) NOT NULL,
-  telp VARCHAR(14) NOT NULL,
-  status INT(1) NOT NULL,
-  CONSTRAINT pk_kasir PRIMARY KEY(ID)
-);
--- STATUS:
---  0 = BUKAN MEMBER
---  1 = MEMBER
-CREATE TABLE pelanggan (
-  ID VARCHAR(12) NOT NULL,
-  nama VARCHAR(50) NOT NULL,
-  telp VARCHAR(14) NOT NULL,
-  status INT(1) NOT NULL,
-  alamat TEXT NOT NULL,
-  CONSTRAINT pk_pelanggan PRIMARY KEY(ID)
-);
--- STATUS:
---  0 = ADA
---  0 = TIDAK ADA
-CREATE TABLE barang (
-  ID VARCHAR(12) NOT NULL,
-  nama VARCHAR(50) NOT NULL,
-  harga INT(10) NOT NULL,
-  deskripsi TEXT NOT NULL,
-  status INT(1) NOT NULL,
-  CONSTRAINT pk_barang PRIMARY KEY(ID)
-);
-CREATE TABLE pesanan (
-  ID VARCHAR(12) NOT NULL,
-  tanggal DATE NOT NULL,
-  pelanggan VARCHAR(12) NOT NULL,
-  CONSTRAINT pk_pesanan PRIMARY KEY(ID),
-  CONSTRAINT fk_pelanggan FOREIGN KEY(pelanggan) REFERENCES pelanggan(ID)
-);
-CREATE TABLE detail_pesanan (
-  id_pesanan VARCHAR(12) NOT NULL,
-  barang VARCHAR(12) NOT NULL,
-  jumlah INT(5) NOT NULL,
-  CONSTRAINT fk_id_pesanan FOREIGN KEY(id_pesanan) REFERENCES pesanan(ID),
-  CONSTRAINT fk_barang FOREIGN KEY(barang) REFERENCES barang(ID)
-);
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 21, 2022 at 06:14 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
 
-INSERT INTO kasir (ID, password, nama, jk, telp, status) VALUES
-('memet', 'memet123', 'Memet', 'L', '081234567890', 1),
-('ujang', 'ujang321', 'Ujang', 'L', '081234567891', 1);
-INSERT INTO pelanggan (ID, nama, telp, status, alamat) VALUES
-('A001', 'Anonymus', '-', 0, '');
-INSERT INTO barang (ID,  nama, harga, deskripsi, status) VALUES
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `projek_kasir_java`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `ID` varchar(12) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `harga` int(10) NOT NULL,
+  `deskripsi` text NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`ID`, `nama`, `harga`, `deskripsi`, `status`) VALUES
 ('A001', 'Nasi Goreng Spesial', 18000, 'Nasi goreng ditambah dengan telor ceplok dan sosis', 1),
 ('A002', 'Bakso', 15000, 'Bakso', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_pesanan`
+--
+
+CREATE TABLE `detail_pesanan` (
+  `id_pesanan` varchar(12) NOT NULL,
+  `barang` varchar(12) NOT NULL,
+  `jumlah` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kasir`
+--
+
+CREATE TABLE `kasir` (
+  `ID` varchar(12) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `jk` char(1) NOT NULL,
+  `telp` varchar(14) NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kasir`
+--
+
+INSERT INTO `kasir` (`ID`, `password`, `nama`, `jk`, `telp`, `status`) VALUES
+('memet', 'memet123', 'Memet', 'L', '081234567890', 1),
+('ujang', 'ujang321', 'Ujang', 'L', '081234567891', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelanggan`
+--
+
+CREATE TABLE `pelanggan` (
+  `ID` varchar(12) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `telp` varchar(14) NOT NULL,
+  `status` int(1) NOT NULL,
+  `alamat` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pelanggan`
+--
+
+INSERT INTO `pelanggan` (`ID`, `nama`, `telp`, `status`, `alamat`) VALUES
+('A001', 'Anonymus', '-', 0, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pesanan`
+--
+
+CREATE TABLE `pesanan` (
+  `ID` varchar(12) NOT NULL,
+  `tanggal` date NOT NULL,
+  `pelanggan` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `detail_pesanan`
+--
+ALTER TABLE `detail_pesanan`
+  ADD KEY `fk_id_pesanan` (`id_pesanan`),
+  ADD KEY `fk_barang` (`barang`);
+
+--
+-- Indexes for table `kasir`
+--
+ALTER TABLE `kasir`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `pesanan`
+--
+ALTER TABLE `pesanan`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `fk_pelanggan` (`pelanggan`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `detail_pesanan`
+--
+ALTER TABLE `detail_pesanan`
+  ADD CONSTRAINT `fk_barang` FOREIGN KEY (`barang`) REFERENCES `barang` (`ID`),
+  ADD CONSTRAINT `fk_id_pesanan` FOREIGN KEY (`id_pesanan`) REFERENCES `pesanan` (`ID`);
+
+--
+-- Constraints for table `pesanan`
+--
+ALTER TABLE `pesanan`
+  ADD CONSTRAINT `fk_pelanggan` FOREIGN KEY (`pelanggan`) REFERENCES `pelanggan` (`ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
